d615466f371170150fcb162d56fc1bc4cabb5f44
Suzon999 <ashikurrahamansuzon346112.com>
2022-09-29 20:56:07 +0600

    modal added

--- src/components/Break/Break.js
+++ src/components/Break/Break.js
@@ -1,34 +1,47 @@
 import React, { useEffect, useState } from 'react';
-import './Break.css'
+import './Break.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const Break = ({ list }) => {
+    const [sec, setSec] = useState('');
     let times = 0;
     for (const time of list) {
         times = parseFloat(times + time.time);
 
     }
+    // toast 
+    const notify = () => toast("Wow so easy!");
 
+
+    const handleBreak = (seconds) => {
+        setSec(seconds);
+        // setBreak(seconds)
+    }
 
     return (
         <div className='informatin-list'>
             <h4 className='my-4'>Add A Break</h4>
             <div className='button'>
-                <button className='btn flex-button'>10s</button>
-                <button className='btn flex-button text-center'>20s</button>
-                <button className='btn flex-button'>30s</button>
-                <button className='btn flex-button'>40s</button>
-                <button className='btn flex-button'>50s</button>
+                <button onClick={() => handleBreak('10s')} className='btn flex-button'>10s</button>
+                <button onClick={() => handleBreak('20s')} className='btn flex-button text-center'>20s</button>
+                <button onClick={() => handleBreak('30s')} className='btn flex-button'>30s</button>
+                <button onClick={() => handleBreak('40s')} className='btn flex-button'>40s</button>
+                <button onClick={() => handleBreak('50s')} className='btn flex-button'>50s</button>
             </div>
             <div className='mt-5'>
                 <h4 className=''>Exercise Details</h4>
                 <h5 className='my-4 time'>Exercise time :{times} seconds </h5>
-                <h5 className='time'>Break time :</h5>
+                <h5 className='time'>Break time : {sec ? sec : '0'} </h5>
 
             </div>
-            <button className='btn-cart mt-5'>
+
+            <button onClick={notify} className='btn-cart mt-5'>
                 <h5 className='btn-text'>Activity Completed</h5>
 
             </button>
+            <ToastContainer />
         </div>
     );
 };
